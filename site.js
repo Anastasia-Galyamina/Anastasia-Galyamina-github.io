591cd212e9508bb45368ea59d0b2bf9781a6f2d4
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-12-13 21:04:19 +0400

    Update site.js

--- site.js
+++ site.js
@@ -1,4 +1,55 @@
-let d = "List";
-foreach (form f in document.forms)
-if(name == form)
-  alert(form);
+const form = document.forms[0];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+
+  let Name = data.get('Name'); 
+
+ 
+  const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    
+    swalWithBootstrapButtons.fire({
+      title: 'Добавить?',
+      text: "Добавить новость на сайт?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Да',
+      cancelButtonText: 'Нет',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        swalWithBootstrapButtons.fire(
+          'Добавлено!',
+          'Ваша новость добавлена',
+          'success'
+        )
+        setTimeout(function () {
+            window.location.href = "index.html";
+        }, 1000);
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        swalWithBootstrapButtons.fire(
+          'Отменена',
+          'Не удалось сохранить',
+          'error'
+        )
+      }
+    })
+});
+
+form.addEventListener("formdata", event => {
+    const data = event.formData;
+
+    const entries = [...data.entries()];
+
+    entries.forEach(field => {
+        console.log(field);
+    })
+});
